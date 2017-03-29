7bef61774c9c9e44a638472dfd30de285517f23e
greenylove <sang022@e.ntu.edu.sg>
2017-03-29 15:39:04 +0800

    c

---
+++ WebApplication1/App_Data/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [1] NCHAR(10) NULL, 
+    [2] NCHAR(10) NULL, 
+    [3] NCHAR(10) NULL, 
+    [4] NCHAR(10) NULL, 
+    [5] NCHAR(10) NULL, 
+    [6] NCHAR(10) NULL, 
+    [7] NCHAR(10) NULL
+)
